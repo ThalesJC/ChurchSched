ba67aaa80a9b2eb936cda2e98f8338ebdd6c0d85
ThalesJC <thales.soul96@gmail.com>
2023-05-22 23:28:24 -0300

    feat: implementa context

---
+++ App/src/Components/LoginCard.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function LoginCard() {
+    const [name, setName] = useState<string>('')
+    const [phone, setPhone] = useState<string>('')
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="text" placeholder="Nome" />
+            <input type="text" placeholder="Telefone" />
+            <button>Entrar</button>
+            <button>Cadastre-se</button>
+        </div>
+    )
+}
--- App/src/Pages/Login.tsx
+++ App/src/Pages/Login.tsx
@@ -1,16 +1,9 @@
-import { useState } from "react"
+import LoginCard from "../Components/LoginCard"
 
 export default function Login() {
-    const [name, setName] = useState<string>('')
-    const [phone, setPhone] = useState<string>('')
+    
 
     return (
-        <div>
-            <h1>Login</h1>
-            <input type="text" placeholder="Nome" />
-            <input type="text" placeholder="Telefone" />
-            <button>Entrar</button>
-            <button>Cadastre-se</button>
-        </div>
+        <LoginCard />
     )
 }
