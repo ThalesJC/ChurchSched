d8cd317def58d23dce9adbd91819753a33ff0867
ThalesJC <thales.soul96@gmail.com>
2023-05-25 23:55:23 -0300

    feat:Header component

--- App/src/Pages/HomeUser.tsx
+++ App/src/Pages/HomeUser.tsx
@@ -1,15 +1,13 @@
+import Header from "../Components/Header"
+
+
 export default function HomeUser() {
     return (
-        <div>
-            <div>
-                <button>"Nome"</button>
-                <button>Sair</button>
-            </div>
-            <div>
-                <input type="checkbox" />
-                <p>Comunidade nossa senhora do carmo</p>
-                <p>Sabado | 19:00</p>
-            </div>
+        <div className="bg-blue-800 min-h-screen min-w-full">
+            <Header />
+            <input type="checkbox" />
+            <p>Comunidade nossa senhora do carmo</p>
+            <p>Sabado | 19:00</p>
         </div>
     )
 }
