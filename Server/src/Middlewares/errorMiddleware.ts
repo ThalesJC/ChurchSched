6f1c8f6a3432c9b7ff60df8f704fc7b594bf8fb5
ThalesJC <thales.soul96@gmail.com>
2023-05-30 17:18:02 -0300

    Feat: backend configurado

---
+++ Server/src/Middlewares/errorMiddleware.ts
@@ -0,0 +1,25 @@
+import { ErrorRequestHandler } from 'express';
+
+const errorMiddleware: ErrorRequestHandler = (error, _req, res, next) => {
+  const { name, message, details } = error;
+  console.error(`name: ${name}`);
+
+  switch (name) {
+    case 'validationError':
+      res.status(400).json({ message: details[0].message });
+      break;
+      case 'NotFoundError':
+        res.status(404).json({ message });
+        break;
+      case 'ConflictError':
+        res.status(409).json({ message });
+        break;
+    default:
+      console.log(error);
+      res.sendStatus(500);
+  }
+
+  next();
+};
+
+export default errorMiddleware;
